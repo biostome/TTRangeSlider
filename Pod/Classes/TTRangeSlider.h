cd6cd0d1de34e08228b226c6265b9b67513b4f70
Face <>
2020-05-18 16:32:08 +0800

    刻度图层引用

--- Pod/Classes/TTRangeSlider.h
+++ Pod/Classes/TTRangeSlider.h
@@ -6,6 +6,7 @@
 
 #import <UIKit/UIKit.h>
 #import "TTRangeSliderDelegate.h"
+#import "TTRangeSlider+Calibration.h"
 
 IB_DESIGNABLE
 @interface TTRangeSlider : UIControl <UIGestureRecognizerDelegate>
